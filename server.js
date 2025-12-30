600c4ee698afe7fc1154d1648a17212a5c36bbf3
sarahpengG <sarah.peng.2@gmail.com>
2025-12-29 20:24:45 -0600

    working atgin

--- server.js
+++ server.js
@@ -2,28 +2,29 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 app.set("view engine", "pug");
+app.set("views", "templates");
+const mongoose = require('mongoose');
+
+app.use(express.static("templates"));
+app.use(express.static("assets"));
+
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.render("landing.pug")
 })
-
-app.get('/menu', (req, res) => {
-  res.send('Hello World!')
+app.get('/landing', (req, res) => {
+  res.render("landing.pug")
 })
-
 app.get('/order', (req, res) => {
-  res.send('Hello World!')
+  res.render("order.pug")
 })
-
 app.get('/events', (req, res) => {
-  res.send('Hello World!')
+  res.render("events.pug")
 })
 app.get('/account', (req, res) => {
-  res.send('Hello World!')
+  res.render("account.pug")
 })
-app.get('/login', (req, res) => {
-  res.send('Hello World!')
-})
+
 
 app.listen(PORT, function (err) {
   if (err) console.log("Error in server setup");
